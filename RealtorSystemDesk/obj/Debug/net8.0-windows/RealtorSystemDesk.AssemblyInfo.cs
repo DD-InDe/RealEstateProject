c84c0a4c67227aa7d461828d13021a9190fb4895
deer <bogdanovich_7712@mail.ru>
2025-01-10 04:31:07 +0300

    merging

--- RealtorSystemDesk/obj/Debug/net8.0-windows/RealtorSystemDesk.AssemblyInfo.cs
+++ RealtorSystemDesk/obj/Debug/net8.0-windows/RealtorSystemDesk.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("RealtorSystemDesk")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+04ac04d35edb8dc14280ed295a24e4abaa29d7e5")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+6cd511397ea2ee9a672baa5a01e0553997e65907")]
 [assembly: System.Reflection.AssemblyProductAttribute("RealtorSystemDesk")]
 [assembly: System.Reflection.AssemblyTitleAttribute("RealtorSystemDesk")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
